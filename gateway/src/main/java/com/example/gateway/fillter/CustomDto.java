384ddb5b6c344a9d5cbc2f2832e75b9df70c07fb
Jeongsu6637 <wjdtn8738@gmail.com>
2023-09-18 10:03:46 +0900

    test

---
+++ gateway/src/main/java/com/example/gateway/fillter/CustomDto.java
@@ -0,0 +1,18 @@
+package com.example.gateway.fillter;
+
+public class CustomDto {
+    private boolean logging;
+
+    public CustomDto(boolean isLogging) {
+        this.logging = isLogging;
+    }
+
+    public void CustomDto(){
+
+    }
+
+    public boolean getLogging() {
+        return logging;
+    }
+
+}
